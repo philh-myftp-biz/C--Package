29f0abb6e0c639c2ecd462caa066ea680c739b21
Phil Hunt <philiphuntiv2@gmail.com>
2026-09-17 14:26:54 -0400

    Create get_card.hpp

--- pciutils/pciutils.hpp
+++ pciutils/pciutils.hpp
@@ -1,14 +1,11 @@
 #pragma once
 
-#include <iostream>
-
-extern "C" {
-    #include "pciutils/pci.h"
-}
-
 // 1. Tell Visual Studio to link your custom static library relative to this header file
 #pragma comment(lib, __FILE__ "\\..\\pci.lib")
 
 // 2. Force link the missing Windows OS modules needed by pciutils 
 #pragma comment(lib, "cfgmgr32.lib")
 #pragma comment(lib, "advapi32.lib")
+
+// 3. Include the clean namespaced implementation file
+#include "pciutils/get_card.hpp"
